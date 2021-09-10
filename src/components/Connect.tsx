06b1f49099cc78847f0034723a27eaf6d4c5886c
Matthew Tanner <115193+matthew-tanner@users.noreply.github.com>
2021-09-09 19:19:08 -0500

    installed material ui and test socketio connection

---
+++ src/components/Connect.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { socket } from "../App";
+
+class Connect extends Component {
+  componentDidMount() {
+    // socket.emit("message", "Holy Crap");
+    // socket.emit("register", {email: "matthew@matthew.com", password: "test", displayName: "Matthew"}, (response: any) =>{
+    //   console.log(response);
+    // })
+  }
+  render() {
+    return <div>test</div>;
+  }
+}
+
+export default Connect;
